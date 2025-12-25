144954a26bd7fadc66943fa3ad9ebab6e013d900
NuraliyevUT <nuraliyevumid49@gmail.com>
2025-12-25 08:38:47 +0500

    home and products are ready

---
+++ main.js
@@ -0,0 +1,41 @@
+const url = "https://fakestoreapi.com/products";
+const elProductHome = document.querySelector(".product-grid-home");
+const elProduct = document.querySelector(".product-grid");
+const elLoading = document.querySelector(".loading");
+
+axios.get(url).then((res) => {
+  const data = res.data;
+  showProductHome(data.slice(0, 4));
+  showProduct(data);
+});
+
+function showProductHome(data) {
+  data.map((item) => {
+    elProductHome.innerHTML += `
+    <div class="card">
+          <img
+            src="${item.image}"
+            alt="${item.category}"
+          />
+          <h4>${item.title}</h4>
+          <p class="category">${item.category}</p>
+          <span class="price">$${item.price}</span>
+    </div>`;
+  });
+}
+
+showProduct(data);
+function showProduct(data) {
+  data.map((item) => {
+    elProduct.innerHTML += `
+      <div class="card">
+            <img
+              src="${item.image}"
+              alt="${item.category}"
+            />
+            <h4>${item.title}</h4>
+            <p class="category">${item.category}</p>
+            <span class="price">$${item.price}</span>
+      </div>`;
+  });
+}
